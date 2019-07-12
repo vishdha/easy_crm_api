368d4b5a9bb0c80a9f93190dc3d116add2a882e0
vishal <vishaldhayagude09@gmail.com>
2019-07-12 18:42:56 +0530

    Angular Added for easy crm for routing api with django

---
+++ ngsimplecrm/src/app/app-routing.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AccountListComponent } from './account-list/account-list.component';
+import { AccountCreateComponent } from './account-create/account-create.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { ContactCreateComponent } from './contact-create/contact-create.component';
+import { ActivityListComponent } from './activity-list/activity-list.component';
+import { ActivityCreateComponent } from './activity-create/activity-create.component';
+
+const routes: Routes = [
+{
+    path: 'accounts',
+    component: AccountListComponent
+  },
+  {
+    path: 'create-account',
+    component: AccountCreateComponent
+  },
+  {
+    path: 'contacts',
+    component: ContactListComponent
+  },
+  {
+    path: 'create-contact',
+    component: ContactCreateComponent
+  },
+  {
+    path: 'activities',
+    component: ActivityListComponent
+  },
+  {
+    path: 'create-activity',
+    component: ActivityCreateComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
---
+++ ngsimplecrm/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from  '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AccountListComponent } from './account-list/account-list.component';
+import { AccountCreateComponent } from './account-create/account-create.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { ContactCreateComponent } from './contact-create/contact-create.component';
+import { ActivityListComponent } from './activity-list/activity-list.component';
+import { ActivityCreateComponent } from './activity-create/activity-create.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AccountListComponent,
+    AccountCreateComponent,
+    ContactListComponent,
+    ContactCreateComponent,
+    ActivityListComponent,
+    ActivityCreateComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
